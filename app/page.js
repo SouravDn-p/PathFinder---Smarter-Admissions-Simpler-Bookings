afa951e2c1f821f97990c934bcf0a6e20946afeb
Sourav-Dn-p <sdsouravdebnath26@gmail.com>
2025-07-10 14:50:57 +0600

    first commit

---
+++ app/page.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+     <h1 className="text-4xl font-bold border p-4 rounded-xl border-green-500 border-dashed bg-amber-950">Path Finder</h1>
+    </div>
+  );
+}
